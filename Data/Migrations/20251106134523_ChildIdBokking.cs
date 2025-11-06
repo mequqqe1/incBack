70470679a434e9b1da1ca089a3198cc679e0a151
mequqqe1 <dauletturlykoza33@gmail.com>
2025-11-06 18:48:13 +0500

    addchildid

---
+++ Data/Migrations/20251106134523_ChildIdBokking.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INCBack.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChildIdBokking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ChildId",
+                table: "bookings",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookings_ChildId",
+                table: "bookings",
+                column: "ChildId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_bookings_children_ChildId",
+                table: "bookings",
+                column: "ChildId",
+                principalTable: "children",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_bookings_children_ChildId",
+                table: "bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_bookings_ChildId",
+                table: "bookings");
+
+            migrationBuilder.DropColumn(
+                name: "ChildId",
+                table: "bookings");
+        }
+    }
+}
